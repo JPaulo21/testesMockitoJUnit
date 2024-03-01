0968ef56533fd06897a6cd30b2810afcb0022b2b
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2024-03-01 13:24:02 -0300

    refactor: Atualizado endpoint de consultar por id

--- src/main/java/com/jp/testesMockito/controller/UserController.java
+++ src/main/java/com/jp/testesMockito/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.jp.testesMockito.controller;
 
 import com.jp.testesMockito.domain.User;
+import com.jp.testesMockito.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().build();
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
