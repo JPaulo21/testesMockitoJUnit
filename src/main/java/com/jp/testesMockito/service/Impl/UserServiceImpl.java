1667e6f2ee0c4f5f5512cbcf8081c51d35a827b6
João Paulo Lira de Almeida <jp.sport17@gmail.com>
2024-03-01 21:02:21 -0300

    feat: Criado expection para objeto não encontrado e aplicada ao User

--- src/main/java/com/jp/testesMockito/service/Impl/UserServiceImpl.java
+++ src/main/java/com/jp/testesMockito/service/Impl/UserServiceImpl.java
@@ -3,10 +3,9 @@ package com.jp.testesMockito.service.Impl;
 import com.jp.testesMockito.domain.User;
 import com.jp.testesMockito.repositories.UserRepository;
 import com.jp.testesMockito.service.UserService;
+import com.jp.testesMockito.service.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +16,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         return userRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Usuário não encontrado")
+                () -> new ObjectNotFoundException("Usuário não encontrado")
         );
     }
 
